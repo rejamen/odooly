2971d67ee6d20028f8e303325fba57610566f1c3
Reinaldo J. Menendez <rejamen@gmail.com>
2025-04-28 22:31:43 +0200

    [ADD] Setup information

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup()
+setup(
+    name="odooly",
+    version="2.1.10",
+    py_modules=["odooly"],
+    author="tinyerp",
+    author_email="tiny@erp.com",
+    maintainer="Reinaldo J.",
+    maintainer_email="rejamen@gmail.com",
+    description="A Python client for Odoo (forked from original repo)",
+    url="https://github.com/rejamen/odooly.git",
+    project_urls={
+        "Original Repository": "https://github.com/tinyerp/odooly.git",
+        "Fork Repository": "https://github.com/rejamen/odooly.git",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
